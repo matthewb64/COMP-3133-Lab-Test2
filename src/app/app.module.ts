4f6e7f54f5de25fc53ad88b69a5de8f35571af68
Your Name <you@example.com>
2023-04-04 14:44:27 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionlistComponent
   ],
   imports: [
     BrowserModule,
